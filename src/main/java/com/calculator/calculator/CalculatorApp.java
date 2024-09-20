6e3a0ece240f5a6e20633f6b29b130ca603cd432
Const <shimanoffsky@gmail.com>
2024-09-20 19:24:16 +0300

    #2 Ivan review comment + 1

--- src/main/java/com/calculator/calculator/CalculatorApp.java
+++ src/main/java/com/calculator/calculator/CalculatorApp.java
@@ -18,7 +18,7 @@ public class CalculatorApp extends Application {
 
     public static void main(String[] args) {
         launch(args);
-        // comment
+        // comment + 1
     }
 
     @Override
